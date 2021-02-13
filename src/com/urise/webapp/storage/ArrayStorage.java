92f1af191a78c6b89b2217b8d653532f0b289edb
Александр Степенко <alexandrstepenko@gmail.com>
2021-02-13 19:54:05 +0300

    Добавил интерфейс, зафиналил размер storage

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,8 +8,9 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10_000];
+public class ArrayStorage implements Storage {
+    private static final int STORAGE_LIMIT = 10_000;
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
@@ -31,7 +32,7 @@ public class ArrayStorage {
     }
 
     private boolean enoughSpace() {
-        if (size < storage.length) {
+        if (size < STORAGE_LIMIT) {
             return true;
         } else {
             System.out.println("В хранилище закончилось место.");
