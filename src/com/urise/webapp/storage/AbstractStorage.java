1794d9a602894f372a33d41ef77c86d59558a195
Александр Степенко <alexandrstepenko@gmail.com>
2021-04-21 23:53:13 +0300

    Добавил логгинг

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -5,8 +5,11 @@ import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
     protected abstract boolean isExist(SK key);
 
     protected abstract SK getSearchKey(String uuid);
@@ -24,6 +27,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     protected abstract List<Resume> toList();
 
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         String uuid = resume.getUuid();
         SK searchKey = searchNotExistKey(uuid);
         saveToStorage(resume, searchKey);
@@ -34,10 +38,12 @@ public abstract class AbstractStorage<SK> implements Storage {
         SK searchKey = getSearchKey(uuid);
         if (!isExist(searchKey))
             return searchKey;
+        LOG.warning("Resume with " + uuid + " already exist");
         throw new ExistStorageException(uuid);
     }
 
     public final Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         return getResume(searchExistKey(uuid));
     }
 
@@ -45,26 +51,31 @@ public abstract class AbstractStorage<SK> implements Storage {
         SK searchKey = getSearchKey(uuid);
         if (isExist(searchKey))
             return searchKey;
+        LOG.warning("There is no resume with " + uuid);
         throw new NotExistStorageException(uuid);
     }
 
     public final void update(Resume resume) {
+        LOG.info("Update " + resume);
         String uuid = resume.getUuid();
         updateResume(resume, searchExistKey(uuid));
         System.out.println("You have updated resume with " + uuid + " " + resume.getFullName());
     }
 
     public final void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         deleteResume(searchExistKey(uuid));
         System.out.println("You have deleted resume with " + uuid);
     }
 
     public final void clear() {
+        LOG.info("Clear");
         clearStorage();
         System.out.println("You have cleared the resume storage.");
     }
 
     public final List<Resume> getAllSorted() {
+        LOG.info("GetAllSorted");
         List<Resume> sortedList = toList();
         sortedList.sort(Resume::compareTo);
         return sortedList;
