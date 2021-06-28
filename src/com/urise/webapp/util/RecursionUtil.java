f874c72cda27f8f03faeb60007e3e70f2725c1d3
Александр Степенко <alexandrstepenko@gmail.com>
2021-06-28 20:19:56 +0300

    Рекурсивный вывод директорий и файлов с отступами

--- src/com/urise/webapp/util/RecursionUtil.java
+++ src/com/urise/webapp/util/RecursionUtil.java
@@ -6,15 +6,20 @@ import java.util.Objects;
 //Базис рекурсии: String path (директория basejava)
 //Точка выхода из рекурсии: окончание цикла for (проход по файлам и директориям basejava)
 public class RecursionUtil {
+    private static StringBuilder tab = new StringBuilder("");
+
     public static void pathOutput(String path) {
-        File root = new File(path);
-        File[] catalog = root.listFiles();
+        File[] catalog = new File(path).listFiles();
+
         for (File file : Objects.requireNonNull(catalog)) {
+            System.out.print(tab.toString());
             if (file.isFile()) {
-                System.out.println("\t" + file.getName());
-            } else if(file.isDirectory()){
                 System.out.println(file.getName());
+            } else if (file.isDirectory()) {
+                System.out.println(file.getName());
+                tab.append("\t");
                 pathOutput(path + File.separator + file.getName());
+                tab.delete(tab.length() - 1, tab.length());
             }
         }
     }
